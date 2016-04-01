7c1039223f3d0d7cfca9d347aff8a2eb946d7fe1
Charisa <mustang1194@gmail.com>
2016-03-31 20:47:36 -0400

    migration added

--- models.py
+++ models.py
@@ -1,8 +1,7 @@
 from app import db
-#from project import bcrypt
 
-#from sqlalchemy import ForeignKey
-#from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class BlogPost(db.Model):
@@ -12,16 +11,33 @@ class BlogPost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     description = db.Column(db.String, nullable=False)
-    #author_id = db.Column(db.Integer, ForeignKey('users.id'))
+    author_id = db.Column(db.Integer, ForeignKey('users.id'))
 
-    #def __init__(self, title, description, author_id):
     def __init__(self, title, description):
         self.title = title
         self.description = description
-        #self.author_id = author_id
 
     def __repr__(self):
-        return '<{}>'.format(self.title)
+        return '<title {}'.format(self.title)
+
+
+class User(db.Model):
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    posts = relationship("BlogPost", backref="author")
+
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        return '<name {}'.format(self.name)
 
 
 '''class User(db.Model):
