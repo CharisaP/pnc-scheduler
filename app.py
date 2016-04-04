b7ede0909116605c8b1ef9f20f23ca82ff3d7841
CharisaP <mustang1194@gmail.com>
2016-04-03 22:03:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 # import the Flask class from the flask module
+
 from flask import Flask, render_template, redirect, \
-    url_for, request, session, flash, jsonify
+    url_for, request, session, flash, jsonify, g
 from flask.ext.sqlalchemy import SQLAlchemy
 from functools import wraps
 import json
-#import sqlite3
+import sqlite3
 
 # create the application object
 app = Flask(__name__, static_url_path='')
@@ -13,12 +14,17 @@ app = Flask(__name__, static_url_path='')
 
 import os
 app.config.from_object(os.environ['APP_SETTINGS'])
+app.database = 'sample.db'
+#create connection and cursor
 
 
 # create sqlalchemy object
-db = SQLAlchemy(app)
 
-from models import * #must be done after db is defined
+#db = SQLAlchemy(app)
+
+
+
+#from models import * #must be done after db is defined
 
 # login required decorator
 def login_required(f):
@@ -37,7 +43,11 @@ def login_required(f):
 @login_required
 def home():
 	# return "Hello, World!"  # return a string
-    posts = db.session.query(BlogPost).all()
+    #posts = db.session.query(BlogPost).all() #change this
+    g.db = connect_db()
+    cur = g.db.execute('select * from posts')
+    posts = [dict(title = row[0], description = row[1]) for row in cur.fetchall()]
+    g.db.close()
     return render_template('index.html', posts=posts)  # render a template
 
 
@@ -48,12 +58,14 @@ def welcome():
 @app.route('/sample')
 @login_required
 def sample():
-	return render_template('sample2.html') #create test calendar
+	f = open('templates/sample2.html')
+	return f.read()
+	#return #render_template('sample.html') #create test calendar
 
 
 # route for handling the login page logic
 @app.route('/login', methods=['GET', 'POST'])
-def login():
+def login():  
     error = None
     if request.method == 'POST':
         if (request.form['username'] != 'admin') \
@@ -72,8 +84,11 @@ def logout():
     session.pop('logged_in', None)
     flash('You were logged out.')
     return redirect(url_for('welcome'))
-    
 
+def connect_db():
+   	return sqlite3.connect(app.database)
+    
+'''
 @app.route('/data')
 def return_data():
     pass
@@ -82,15 +97,14 @@ def return_data():
 @app.route('/deletedata')
 def return_data():
     pass
-
+'''
 @app.route('/savedata')
 def return_data():
     event_obj = request.args.get('data')
     # event obj needs to be json
     print event_obj
  
-
-
 # start the server with the 'run()' method
 if __name__ == '__main__':
     app.run()
+
