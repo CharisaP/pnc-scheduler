f6b7dff25696a05c40259a710aaf27870332c126
xerox1028 <donpatch7@gmail.com>
2016-04-18 05:09:44 -0400

    reformatted database so that posts now contain id's and dates

--- db/events.sql
+++ db/events.sql
@@ -1,3 +1,5 @@
-CREATE TABLE "events" ("id" VARCHAR PRIMARY KEY  NOT NULL  UNIQUE , "title" TEXT, "start" DATETIME, "end" DATETIME, "details" TEXT);
-CREATE TABLE "posts" ("title" VARCHAR NOT NULL , "description" VARCHAR NOT NULL , "author" VARCHAR PRIMARY KEY  NOT NULL );
+BEGIN TRANSACTION;
 CREATE TABLE "users" ("first_name"  NOT NULL , "last_name"  NOT NULL , "id"  PRIMARY KEY  NOT NULL  UNIQUE , "password"  NOT NULL , "username"  NOT NULL  UNIQUE );
+CREATE TABLE "posts" ("id"  VARCHAR PRIMARY KEY  NOT NULL  UNIQUE, "title"	VARCHAR NOT NULL, "description"	VARCHAR NOT NULL,"author"	VARCHAR NOT NULL,"date"	DATETIME NOT NULL);
+CREATE TABLE "events" ("id" VARCHAR PRIMARY KEY  NOT NULL  UNIQUE , "title" TEXT, "start" DATETIME, "end" DATETIME, "details" TEXT);
+COMMIT;
